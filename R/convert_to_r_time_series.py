b808b0d2642f710d95a000e6502b26e0b79f04f5
VitaliKStud <vitali9394@yahoo.de>
2023-08-27 17:46:27 +0200

    convert-to-r-timeseries

--- R/convert_to_r_time_series.py
+++ R/convert_to_r_time_series.py
@@ -4,10 +4,10 @@ import pandas as pd
 def convert_to_r_time_series(data: pd.Series,
                              frequency: int = 12,
                              is_datetime: bool = True,
-                             format: str = "%Y-%m-%d") -> robjects.r["ts"]:
+                             format: str = "%Y-%m-%d %H:%M:%S") -> robjects.r["ts"]:
     """
     data: pd.Series
-    frequency: int
+    frequency: int -> 4: Quarterly, 12 Monthly, 24 Hourly, 60 Minutely, 3600 Secondly
     is_date: Boolean
     format: String
 
@@ -23,7 +23,7 @@ def convert_to_r_time_series(data: pd.Series,
     # Convert the pandas Series index to R Date format
     if is_datetime:
         r_date_vector = robjects.StrVector(data.index.tolist())
-        frequence_index = robjects.r["as.Date"](r_date_vector, format=format)
+        frequence_index = robjects.r['as.POSIXct'](r_date_vector, format=format)
 
     else:
         print("INFORMATION: data.index should be a datetime-format")
@@ -38,12 +38,25 @@ def convert_to_r_time_series(data: pd.Series,
     return ts_data
 
 
-# Example DataFrame
-data = pd.Series([100, 120, 130, 160], index=["1","2","3","5"])
-
-# Convert DataFrame to R time series
-frequency = 10  # Assuming monthly data
-r_time_series = convert_to_r_time_series(data, frequency, is_datetime=True, format="%H")
-
-# Print the R time series
-print(r_time_series)
+# # Example DataFrame
+# data = pd.Series([100, 120, 130, 160,
+#                   100, 120, 130, 160,
+#                   100, 120, 130, 160,
+#                   100, 120, 130, 160,
+#                   100, 120, 130, 160,
+#                   100, 120, 130, 160,
+#                 160
+#                   ],
+#                  index=["2017-01-01 00:00:00", "2017-01-01 01:00:00", "2017-01-01 02:00:00", "2017-01-01 03:00:00",
+#                         "2017-01-01 04:00:00", "2017-01-01 05:00:00", "2017-01-01 06:00:00", "2017-01-01 07:00:00",
+#                         "2017-01-01 08:00:00", "2017-01-01 09:00:00", "2017-01-01 10:00:00", "2017-01-01 11:00:00",
+#                         "2017-01-01 12:00:00", "2017-01-01 13:00:00", "2017-01-01 14:00:00", "2017-01-01 15:00:00",
+#                         "2017-01-01 16:00:00", "2017-01-01 17:00:00", "2017-01-01 18:00:00", "2017-01-01 19:00:00",
+#                         "2017-01-01 20:00:00", "2017-01-01 21:00:00", "2017-01-01 22:00:00", "2017-01-01 23:00:00",
+#                         "2017-01-02 00:00:00"
+#                         ])
+#
+# # Convert DataFrame to R time series
+# frequency = 24  # Assuming monthly data
+# r_time_series = convert_to_r_time_series(data, frequency, is_datetime=True)
+# print(r_time_series)
